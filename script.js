47d2c9caec7638533932ee6d1bff03ce5eac3d77
Artem Chaychenko <drchaychenko@gmail.com>
2025-03-07 19:52:56 +0200

    07.03.25 friday evening

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 
 const modal = document.querySelector('.backdrop')
+const mobile = document.querySelector('.mobile-menu')
+
 function f1() {
     modal.classList.add('is-open')
 } 
 function f2() {
     modal.classList.toggle('is-open')
 }
+function f3 () {
+    mobile.classList.add("on-open")
+}
+function f4 () {
+    mobile.classList.toggle("on-open")
+}
+
+
 document.querySelector('.hero-btn').addEventListener('click', f1);
 document.querySelector('.close-btn').addEventListener('click', f2);
+document.querySelector('.burger-btn').addEventListener('click', f3);
+document.querySelector('.mobile-menu-close-btn').addEventListener('click', f4);
